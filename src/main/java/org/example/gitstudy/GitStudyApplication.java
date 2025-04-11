197fb3405994e7d84bee7f6bcf73656d0aec50fd
parkhayoung <qkrgkdud3010@naver.com>
2025-04-11 17:55:22 +0900

    eter the commit message for your changes. Lines starting

--- src/main/java/org/example/gitstudy/GitStudyApplication.java
+++ src/main/java/org/example/gitstudy/GitStudyApplication.java
@@ -12,7 +12,6 @@ public class GitStudyApplication {
         System.out.println("");
         System.out.println("a");
         System.out.println("b");
-        System.out.println("c");
     }
 
 }
