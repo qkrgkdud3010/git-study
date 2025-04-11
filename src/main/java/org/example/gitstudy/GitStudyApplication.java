e099306eb4047136027b2ff13c16ef64cf41c1fb
parkhayoung <qkrgkdud3010@naver.com>
2025-04-11 16:47:47 +0900

    add new line in main

--- src/main/java/org/example/gitstudy/GitStudyApplication.java
+++ src/main/java/org/example/gitstudy/GitStudyApplication.java
@@ -8,6 +8,7 @@ public class GitStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitStudyApplication.class, args);
+        System.out.println("New line");
     }
 
 }
